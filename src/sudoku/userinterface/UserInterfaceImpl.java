191b49df06e88f145031d21a7828387db6311fe1
JANNOT <evan.jannot@sogeti.com>
2022-12-31 18:49:25 +0100

    Grid lines

--- src/sudoku/userinterface/UserInterfaceImpl.java
+++ src/sudoku/userinterface/UserInterfaceImpl.java
@@ -5,9 +5,11 @@ import javafx.scene.Group;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.shape.Rectangle;
 import sudoku.problemdomain.Coordinates;
 import sudoku.problemdomain.SudokuGame;
 
+import java.awt.*;
 import java.util.HashMap;
 
 public class UserInterfaceImpl implements IUserInterfaceContract.View, EventHandler<KeyEvent> {
@@ -28,14 +30,87 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View, EventHand
     private static final Color BOARD_BACKGROUND_COLOR = Color.rgb(224,242,241);
     private static final String SUDOKU = "Sudoku";
 
-    @Override
-    public void handle(KeyEvent keyEvent) {
-
+    public UserInterfaceImpl(Stage stage) {
+        this.stage = stage;
+        this.root = new Group();
+        this.textFieldCoordinates = new HashMap<>();
+        initializeUserInterface();
     }
+
+    private void initializeUserInterface() {
+        drawBackground(root);
+        drawTitle(root);
+        drawSudokuBoard(root);
+        drawTextFields(root);
+        drawGridLines(root);
+        stage.show();
+    }
+
+    private void drawGridLines(Group root) {
+        int xAndY = 114;
+        int index = 0;
+        while (index < 8) {
+            int thickness;
+            if (index == 2 || index == 5) {
+                thickness = 3;
+            } else {
+                thickness = 2;
+            }
+
+            Rectangle verticalLine = getLine(
+                    xAndY + 64 * index,
+                    BOARD_PADDING,
+                    BOARD_X_AND_Y,
+                    thickness
+            );
+
+            Rectangle horizontalLine = getLine(
+                    BOARD_PADDING,
+                    xAndY + 64 * index,
+                    thickness,
+                    BOARD_X_AND_Y
+            );
+
+            root.getChildren().addAll(
+                    verticalLine,
+                    horizontalLine
+            );
+
+            index++;
+        }
+    }
+
+    private Rectangle getLine(double x, double y, double height, double width) {
+        Rectangle line = new Rectangle();
+        line.setX(x);
+        line.setY(y);
+        line.setHeight(height);
+        line.setWidth(width);
+
+        line.setFill(Color.BLACK);
+        return line;
+    }
+
+    private void drawTextFields(Group root) {
+        
+    }
+
+    private void drawSudokuBoard(Group root) {
+        
+    }
+
+    private void drawTitle(Group root) {
+        
+    }
+
+    private void drawBackground(Group root) {
+        
+    }
+
 
     @Override
     public void setListener(IUserInterfaceContract.EventListener listener) {
-
+        this.listener = listener;
     }
 
     @Override
@@ -55,6 +130,11 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View, EventHand
 
     @Override
     public void showError(String message) {
+
+    }
+
+    @Override
+    public void handle(KeyEvent keyEvent) {
 
     }
 }
